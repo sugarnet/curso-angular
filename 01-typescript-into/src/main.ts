ed6a9ad15d09de384fa138a5c052d299b42adb10
Diego Scifo <diegoscifo@gmail.com>
2024-11-16 20:05:46 -0300

    Reordear carpetas nuevo proyecto intro de typescript

---
+++ 01-typescript-into/src/main.ts
@@ -0,0 +1,9 @@
+import './style.css'
+// import './topics/01-basic-types'
+import './topics/02-object-interface'
+
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+Hola Mundo!!!
+`
+
+console.log("Hola Mundo!!!");
---
+++ 01-typescript-into/src/topics/01-basic-types.ts
@@ -0,0 +1,9 @@
+const name: string = 'Diego';
+let hpPoints: number | 'FULL' = 95;
+const isAlive: boolean = true;
+
+hpPoints = 'FULL';
+
+console.log({
+    name, hpPoints, isAlive
+});
