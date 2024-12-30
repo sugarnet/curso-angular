c2b02dc1ef74fff94da92a6fd9e825f13df682bd
Diego Scifo <diego.scifo@globallogic.com>
2024-12-30 08:24:42 -0300

    Creación de reactive form usando creación tradicional y FormBuilder. Agregar validators.

--- 07-formsApp/src/app/reactive/pages/basic-page/basic-page.component.ts
+++ 07-formsApp/src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -1,9 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   templateUrl: './basic-page.component.html',
   styles: ``
 })
 export class BasicPageComponent {
+
+  // public myForm: FormGroup = new FormGroup({
+  //   name: new FormControl(''),
+  //   price: new FormControl(0),
+  //   inStorage: new FormControl(0),
+  // });
+
+  private fb: FormBuilder = inject(FormBuilder);
+
+  public myForm: FormGroup = this.fb.group({
+    name: ['', [ Validators.required, Validators.minLength(3) ]],
+    price: [0, [ Validators.required, Validators.min(0) ]],
+    inStorage: [0, [ Validators.required, Validators.min(0) ]],
+  });
+  
+  onSave(): void {
+
+    if (this.myForm.invalid) return;
+    
+    console.log(this.myForm.value);
+  }
 
 }
