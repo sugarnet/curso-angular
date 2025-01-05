e0723f3eee78660777a5fd2e9dc9162f0279843d
Diego Scifo <diegoscifo@gmail.com>
2025-01-05 19:35:18 -0300

    validaciones (error al hacer click en crear)

--- 07-formsApp/src/app/auth/pages/register-page/register-page.component.ts
+++ 07-formsApp/src/app/auth/pages/register-page/register-page.component.ts
@@ -1,9 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ValidatorsService } from '../../../shared/services/validators.service';
+// import * as customValidators from '../../../shared/validators/validators';
 
 @Component({
   templateUrl: './register-page.component.html',
   styles: ``
 })
 export class RegisterPageComponent {
+  
+  private fb: FormBuilder = inject(FormBuilder);
+  private validatorService: ValidatorsService = inject(ValidatorsService);
 
+  public myForm: FormGroup = this.fb.group({
+    name: ['', [ Validators.required, Validators.pattern(this.validatorService.firstNameAndLastnamePattern) ]],
+    email: ['', [ Validators.required, Validators.pattern(this.validatorService.emailPattern) ]],
+    username: ['', [ Validators.required, this.validatorService.cantBeStrider ]],
+    password: ['', [ Validators.required, Validators.minLength(6) ]],
+    password2: ['', [ Validators.required ]],
+  });
+
+    onSubmit() {
+      console.log('crear');
+      this.myForm.markAllAsTouched();
+    }
+
+    isValidField(field: string) {
+      this.validatorService.isValidField(this.myForm, field);
+    }
 }
--- 07-formsApp/src/app/reactive/pages/switches-page/switches-page.component.ts
+++ 07-formsApp/src/app/reactive/pages/switches-page/switches-page.component.ts
@@ -1,9 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   templateUrl: './switches-page.component.html',
   styles: ``
 })
-export class SwitchesPageComponent {
+export class SwitchesPageComponent implements OnInit {
+ private fb: FormBuilder = inject(FormBuilder);
 
+  public myForm: FormGroup = this.fb.group({
+    gender: ['M', [ Validators.required ]],
+    wantNotifications: [true, [ Validators.required ]],
+    termsAndConditions: [false, [ Validators.requiredTrue ]],
+  });
+
+  public person = {gender: 'F', wantNotifications: false};
+
+  ngOnInit(): void {
+    this.myForm.reset(this.person);
+  }
+
+  onSubmit(): void {
+    if (this.myForm.invalid) {
+      this.myForm.markAllAsTouched();
+      return; 
+    }
+
+    const {termsAndConditions, ...newPerson} = this.myForm.value;
+    this.person = newPerson;
+    
+    console.log(this.myForm.value);
+    console.log(newPerson);
+  }
+
+  isValidField(field: string): boolean | null {
+    return this.myForm.controls[field].errors 
+      && this.myForm.controls[field].touched;
+  }
 }
