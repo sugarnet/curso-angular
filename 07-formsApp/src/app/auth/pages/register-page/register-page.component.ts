653ff0967debd74e479fc35e99ff00428362eeaf
Diego Scifo <diego.scifo@globallogic.com>
2025-01-06 09:58:13 -0300

    resolver error de mostrar errores

--- 07-formsApp/src/app/auth/pages/register-page/register-page.component.ts
+++ 07-formsApp/src/app/auth/pages/register-page/register-page.component.ts
@@ -1,6 +1,7 @@
 import { Component, inject } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ValidatorsService } from '../../../shared/services/validators.service';
+import { EmailValidator } from '../../../shared/validators/email-validator.service';
 // import * as customValidators from '../../../shared/validators/validators';
 
 @Component({
@@ -11,21 +12,22 @@ export class RegisterPageComponent {
   
   private fb: FormBuilder = inject(FormBuilder);
   private validatorService: ValidatorsService = inject(ValidatorsService);
+  private emailValidator: EmailValidator = inject(EmailValidator);
 
   public myForm: FormGroup = this.fb.group({
     name: ['', [ Validators.required, Validators.pattern(this.validatorService.firstNameAndLastnamePattern) ]],
-    email: ['', [ Validators.required, Validators.pattern(this.validatorService.emailPattern) ]],
+    // email: ['', [ Validators.required, Validators.pattern(this.validatorService.emailPattern) ], [new EmailValidator()]],
+    email: ['', [ Validators.required, Validators.pattern(this.validatorService.emailPattern) ], [this.emailValidator]],
     username: ['', [ Validators.required, this.validatorService.cantBeStrider ]],
     password: ['', [ Validators.required, Validators.minLength(6) ]],
     password2: ['', [ Validators.required ]],
   });
 
-    onSubmit() {
-      console.log('crear');
-      this.myForm.markAllAsTouched();
-    }
+  onSubmit() {
+    this.myForm.markAllAsTouched();
+  }
 
-    isValidField(field: string) {
-      this.validatorService.isValidField(this.myForm, field);
-    }
+  isValidField(field: string) {
+    return this.validatorService.isValidField(this.myForm, field);
+  }
 }
