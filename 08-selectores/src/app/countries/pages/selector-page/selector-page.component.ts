bc7ac5388839768ca730e084f53a7b82568a8e1c
Diego Scifo <diego.scifo@globallogic.com>
2025-01-07 07:38:27 -0300

    nuevo proyecto sección 19. resolver tarea de routing.

---
+++ 08-selectores/src/app/countries/pages/selector-page/selector-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-selector-page',
+  standalone: false,
+  
+  templateUrl: './selector-page.component.html',
+  styles: ``
+})
+export class SelectorPageComponent {
+
+}
