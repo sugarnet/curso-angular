6d51a303e5f1a3658f223dfbcbe96d4d35e87afd
Diego Scifo <diego.scifo@globallogic.com>
2025-02-04 10:24:50 -0300

    Presentar mapa.

--- 10-mapsApp/src/app/maps/maps.module.ts
+++ 10-mapsApp/src/app/maps/maps.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import mapboxgl from 'mapbox-gl'; // or "const mapboxgl = require('mapbox-gl');"
+
 import { MapsRoutingModule } from './maps-routing.module';
 import { MiniMapComponent } from './components/mini-map/mini-map.component';
 import { SideMenuComponent } from './components/side-menu/side-menu.component';
@@ -10,6 +12,9 @@ import { MarkersPageComponent } from './pages/markers-page/markers-page.componen
 import { PropertiesPageComponent } from './pages/properties-page/properties-page.component';
 import { ZoomRangePageComponent } from './pages/zoom-range-page/zoom-range-page.component';
 
+import { environment } from '../../environments/environments';
+
+mapboxgl.accessToken = environment.mapbox_token;
 
 @NgModule({
   declarations: [
---
+++ 10-mapsApp/src/environments/environments.ts
@@ -0,0 +1,5 @@
+
+export const environment = {
+    mapbox_token: "pk.eyJ1IjoiZGllZ29zY2lmbyIsImEiOiJjazVjN3llbDgweGFlM25yejFjenFlOTJvIn0.s9Agjv2njK6YrDcCqWbbOA",
+    other: "PROPERTY"
+};
