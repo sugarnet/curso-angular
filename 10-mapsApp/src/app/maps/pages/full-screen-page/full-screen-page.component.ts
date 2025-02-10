3811aba0193c5c7e6d2813854ee08e863270ced1
Diego Scifo <diego.scifo@globallogic.com>
2025-02-10 08:51:12 -0300

    agregar marcadores y guardar en local storage

--- 10-mapsApp/src/app/maps/pages/full-screen-page/full-screen-page.component.ts
+++ 10-mapsApp/src/app/maps/pages/full-screen-page/full-screen-page.component.ts
@@ -17,7 +17,7 @@ export class FullScreenPageComponent implements AfterViewInit {
     const map = new Map({
       container: this.divMap.nativeElement, // container ID
       style: 'mapbox://styles/mapbox/streets-v12', // style URL
-      center: [-74.5, 40], // starting position [lng, lat]
+      center: [-68.84444444444404, -32.88972222222181], // starting position [lng, lat]
       zoom: 9, // starting zoom
     });
 
