75041c3099e0a5af2f8fe063faaccff9e987e827
Diego Scifo <diego.scifo@globallogic.com>
2025-05-16 06:39:29 -0300

    Agregar Counter component

---
+++ 02-bases/src/app/pages/counter/counter.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  template: `
+    <h1>Hola Mundo!!</h1>
+    <h2>CounterPageComponent...</h2>
+  `,
+})
+export class CounterPageComponent {}
