68238fb30560f765aed8aee11e6e7b54fd8508fa
Diego Scifo <diegoscifo@gmail.com>
2025-08-31 11:12:44 -0300

    Implementación de debounce

--- 04-country-app/src/app/country/components/country-search-input/country-search-input.component.ts
+++ 04-country-app/src/app/country/components/country-search-input/country-search-input.component.ts
@@ -1,8 +1,10 @@
 import {
   ChangeDetectionStrategy,
   Component,
+  effect,
   input,
   output,
+  signal,
 } from '@angular/core';
 
 @Component({
@@ -14,4 +16,19 @@ import {
 export class CountrySearchInputComponent {
   value = output<string>();
   placeholder = input.required<string>();
+  debounceTime = input<number>(300);
+
+  inputValue = signal<string>('');
+
+  debounceEffect = effect((onCleanup) => {
+    const value = this.inputValue();
+
+    const timeout = setTimeout(() => {
+      this.value.emit(value);
+    }, this.debounceTime());
+
+    onCleanup(() => {
+      clearTimeout(timeout);
+    });
+  });
 }
--- 04-country-app/src/app/country/services/country.service.ts
+++ 04-country-app/src/app/country/services/country.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
-import { catchError, map, Observable, throwError } from 'rxjs';
+import { catchError, map, Observable, of, throwError } from 'rxjs';
 import { Country } from '../interfaces/country.interface';
 import { RESTCountry } from '../interfaces/rest-country.interface';
 import { CountryMapper } from '../mappers/country.mapper';
