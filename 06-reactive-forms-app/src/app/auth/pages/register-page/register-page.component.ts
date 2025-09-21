b82a3821f8f67c1f8ed74b6a899c604a9888c725
Diego Scifo <diegoscifo@gmail.com>
2025-09-20 21:06:07 -0300

    Inicio sección 15. Comienzo actividad (incompleta)

--- 06-reactive-forms-app/src/app/auth/pages/register-page/register-page.component.ts
+++ 06-reactive-forms-app/src/app/auth/pages/register-page/register-page.component.ts
@@ -1,9 +1,33 @@
 import { JsonPipe } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { FormUtils } from '../../../utils/form-utils';
 
 @Component({
   selector: 'app-register-page',
-  imports: [JsonPipe],
+  imports: [JsonPipe, ReactiveFormsModule],
   templateUrl: './register-page.component.html',
 })
-export class RegisterPageComponent {}
+export class RegisterPageComponent {
+  private fb = inject(FormBuilder);
+
+  myForm: FormGroup = this.fb.group({
+    name: ['', [Validators.required]],
+    email: ['', [Validators.required, Validators.email]],
+    username: ['', Validators.required, Validators.minLength(6)],
+    password1: ['', Validators.required, Validators.minLength(6)],
+    password2: ['', Validators.required],
+  });
+
+  formUtils = FormUtils;
+
+  onSubmit() {
+    console.log(this.myForm.value);
+    this.myForm.markAllAsTouched();
+  }
+}
--- 06-reactive-forms-app/src/app/reactive/pages/switches-page/switches-page.component.ts
+++ 06-reactive-forms-app/src/app/reactive/pages/switches-page/switches-page.component.ts
@@ -1,9 +1,31 @@
 import { JsonPipe } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { FormUtils } from '../../../utils/form-utils';
 
 @Component({
   selector: 'app-switches-page',
-  imports: [JsonPipe],
+  imports: [JsonPipe, ReactiveFormsModule],
   templateUrl: './switches-page.component.html',
 })
-export class SwitchesPageComponent {}
+export class SwitchesPageComponent {
+  private fb = inject(FormBuilder);
+  formUtils = FormUtils;
+
+  myForm: FormGroup = this.fb.group({
+    // como valor por default puedo poner null o directamente nada
+    gender: [, Validators.required],
+    wantNotifications: [true],
+    termsAndConditions: [false, Validators.requiredTrue],
+  });
+
+  onSubmit() {
+    console.log(this.myForm.value);
+    this.myForm.markAllAsTouched();
+  }
+}
