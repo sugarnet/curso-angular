717b501e5dcb2a9e1f3bb4458604ce221378471a
Diego Scifo <diego.scifo@globallogic.com>
2025-10-18 08:22:21 -0300

    Fin Sección 18. Implementar Carousel.

--- 09-teslo-shop/src/app/store-front/pages/gender-page/gender-page.component.ts
+++ 09-teslo-shop/src/app/store-front/pages/gender-page/gender-page.component.ts
@@ -1,8 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { rxResource, toSignal } from '@angular/core/rxjs-interop';
+import { ActivatedRoute } from '@angular/router';
+import { map } from 'rxjs';
+
+import { ProductsService } from '@products/services/products.service';
+import { ProductCardComponent } from '@products/components/product-card/product-card.component';
 
 @Component({
   selector: 'app-gender-page',
-  imports: [],
+  imports: [ProductCardComponent],
   templateUrl: './gender-page.component.html',
 })
-export class GenderPageComponent { }
+export default class GenderPageComponent {
+  route = inject(ActivatedRoute);
+
+  gender = toSignal(this.route.params.pipe(map(({ gender }) => gender)));
+
+  productsService = inject(ProductsService);
+
+  productsResource = rxResource({
+    params: () => ({ gender: this.gender() }),
+    stream: ({ params }) => {
+      return this.productsService.getProducts({ gender: params.gender });
+    },
+  });
+}
--- 09-teslo-shop/src/app/store-front/store-front.routes.ts
+++ 09-teslo-shop/src/app/store-front/store-front.routes.ts
@@ -1,7 +1,7 @@
 import { Routes } from '@angular/router';
 import { StoreFrontLayoutComponent } from './layouts/store-front-layout/store-front-layout.component';
 import { HomePageComponent } from './pages/home-page/home-page.component';
-import { GenderPageComponent } from './pages/gender-page/gender-page.component';
+import GenderPageComponent from './pages/gender-page/gender-page.component';
 import { ProductPageComponent } from './pages/product-page/product-page.component';
 import { NotFoundPageComponent } from './pages/not-found-page/not-found-page.component';
 
