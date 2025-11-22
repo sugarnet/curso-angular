4b5d46582cef527d31f02995cd7a062047bc7705
Diego Scifo <diegoscifo@yahoo.com.ar>
2025-11-22 18:06:12 -0300

    Agregar backend nuevamente para reintento exitoso de despliegue

--- 09-teslo-shop/src/app/app.config.ts
+++ 09-teslo-shop/src/app/app.config.ts
@@ -5,22 +5,19 @@ import {
 } from '@angular/core';
 import { provideRouter, withHashLocation } from '@angular/router';
 
-import { routes } from './app.routes';
 import {
   provideHttpClient,
   withFetch,
   withInterceptors,
 } from '@angular/common/http';
-import { loggingInterceptor } from '@shared/interceptors/logging.interceptor';
 import { authInterceptor } from '@auth/interceptors/auth.interceptor';
-import { HashLocationStrategy, LocationStrategy } from '@angular/common';
+import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
-    provideRouter(routes),
-    { provide: LocationStrategy, useClass: HashLocationStrategy },
+    provideRouter(routes, withHashLocation()),
     provideHttpClient(
       withFetch(),
       withInterceptors([
