20733f78a4f5a413ac22be1e5ef726e9ac6d6f74
Diego Scifo <diego.scifo@globallogic.com>
2025-12-06 21:02:54 -0300

    feat: enhance calculator button component with pressed state and keyboard event handling

--- _pro/01-zoneless-calculator/src/app/calculator/components/calculator-button/calculator-button.component.ts
+++ _pro/01-zoneless-calculator/src/app/calculator/components/calculator-button/calculator-button.component.ts
@@ -2,9 +2,13 @@ import {
   Attribute,
   ChangeDetectionStrategy,
   Component,
+  ElementRef,
   HostBinding,
   input,
   OnInit,
+  output,
+  signal,
+  viewChild,
   ViewEncapsulation,
 } from '@angular/core';
 
@@ -15,13 +19,21 @@ import {
   styleUrl: './calculator-button.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
   host: {
-    class: 'w-1/4 border-r border-b border-indigo-400',
+    class: 'border-r border-b border-indigo-400',
+    '[class.w-2/4]': 'isDoubleSize()',
+    '[class.w-1/4]': '!isDoubleSize()',
     // attribute: 'hola',
     // 'data-size': '12',
+    // https://angular.dev/guide/components/host-elements
   },
   // encapsulation: ViewEncapsulation.None,
 })
 export class CalculatorButtonComponent {
+  public isPressed = signal(false);
+
+  public onClick = output<string>();
+  public contentValue = viewChild<ElementRef<HTMLButtonElement>>('button');
+
   public isCommand = input(false, {
     transform: this.booleanTransform(),
   });
@@ -37,7 +49,30 @@ export class CalculatorButtonComponent {
   //   return this.isCommand();
   // }
 
-  @HostBinding('class.w-2/4') get commandStyle() {
-    return this.isDoubleSize();
+  // @HostBinding('class.w-2/4') get commandStyle() {
+  //   return this.isDoubleSize();
+  // }
+
+  handleClick() {
+    if (!this.contentValue()?.nativeElement) {
+      return;
+    }
+
+    const value = this.contentValue()!.nativeElement.innerText;
+    this.onClick.emit(value.trim());
+  }
+
+  public keyboardPressedStyle(key: string) {
+    if (!this.contentValue()) return;
+
+    const value = this.contentValue()!.nativeElement.innerText;
+
+    if (value !== key) return;
+
+    this.isPressed.set(true);
+
+    setTimeout(() => {
+      this.isPressed.set(false);
+    }, 100);
   }
 }
--- _pro/01-zoneless-calculator/src/app/calculator/components/calculator/calculator.component.ts
+++ _pro/01-zoneless-calculator/src/app/calculator/components/calculator/calculator.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, viewChildren } from '@angular/core';
 import { CalculatorButtonComponent } from '../calculator-button/calculator-button.component';
 
 @Component({
@@ -11,9 +11,35 @@ import { CalculatorButtonComponent } from '../calculator-button/calculator-butto
     .is-command {
     @apply bg-indigo-700 opacity-20;*/
   `,
+  host: {
+    '(document:keyup)': 'handleKeyboardEvent($event)',
+  },
 })
 export class CalculatorComponent {
+  public calculatorButtons = viewChildren(CalculatorButtonComponent);
+
   handleClick(key: string) {
     console.log({ key });
+  }
+
+  //@HostListener('document:keyup', ['$event'])
+  handleKeyboardEvent(event: KeyboardEvent) {
+    const keyEquivalents: Record<string, string> = {
+      Escape: 'C',
+      Clear: 'C',
+      '*': 'x',
+      '/': '÷',
+      Enter: '=',
+      c: 'C',
+    };
+
+    const key = event.key;
+    const keyValue = keyEquivalents[key] ?? key;
+
+    this.handleClick(keyValue);
+
+    this.calculatorButtons().forEach((button) => {
+      button.keyboardPressedStyle(keyValue);
+    });
   }
 }
