1ee6a0caa9b98ef30f0ad319416ca0ec44c0d71b
Diego Scifo <diego.scifo@globallogic.com>
2025-12-19 07:58:42 -0300

    feat: implement server-side rendering with Angular SSR and configure server routes

--- _pro/02-pokemon-ssr/src/app/app.config.ts
+++ _pro/02-pokemon-ssr/src/app/app.config.ts
@@ -1,12 +1,18 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
+import {
+  ApplicationConfig,
+  provideBrowserGlobalErrorListeners,
+  provideZonelessChangeDetection,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
-    provideRouter(routes)
-  ]
+    provideRouter(routes),
+    provideClientHydration(withEventReplay()),
+  ],
 };
