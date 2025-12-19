0801e6405281ca8be802b76dc0783c7b1aafabf9
Diego Scifo <diego.scifo@globallogic.com>
2025-12-19 07:43:17 -0300

    Inicio sección 5

---
+++ _pro/02-pokemon-ssr/src/app/shared/components/navbar/navbar.component.ts
@@ -0,0 +1,10 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-navbar',
+  imports: [RouterLink],
+  templateUrl: './navbar.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class NavbarComponent {}
