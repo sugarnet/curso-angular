4cb56cd701e0f8f9cba3a98f50bce08075b18001
Diego Scifo <diego.scifo@globallogic.com>
2025-12-26 08:56:24 -0300

    feat: update redirect location in netlifyCommonEngineHandler to '/pokemons'

--- _pro/02-pokemon-ssr/src/server.ts
+++ _pro/02-pokemon-ssr/src/server.ts
@@ -11,7 +11,7 @@ export async function netlifyCommonEngineHandler(
   if (url.pathname === '/') {
     return new Response(null, {
       status: 302,
-      headers: { Location: '/pokemon-ssr/about' },
+      headers: { Location: '/pokemons' },
     });
   }
   return await render(commonEngine);
