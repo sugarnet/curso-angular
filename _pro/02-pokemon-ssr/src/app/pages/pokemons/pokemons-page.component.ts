7e325b8ab8e90b4b49cfa089930c197bc15111b0
Diego Scifo <diegoscifo@yahoo.com.ar>
2026-01-04 09:49:12 -0300

    feat: add PokemonsService and interfaces for API response handling

--- _pro/02-pokemon-ssr/src/app/pages/pokemons/pokemons-page.component.ts
+++ _pro/02-pokemon-ssr/src/app/pages/pokemons/pokemons-page.component.ts
@@ -1,6 +1,15 @@
-import { ChangeDetectionStrategy, Component, OnInit, signal } from '@angular/core';
+import {
+  ApplicationRef,
+  ChangeDetectionStrategy,
+  Component,
+  inject,
+  OnDestroy,
+  OnInit,
+  signal,
+} from '@angular/core';
 import { PokemonListComponent } from '../../pokemons/components/pokemon-list/pokemon-list.component';
 import { PokemonListSkeletonComponent } from '../../pokemons/components/pokemon-list-skeleton/pokemon-list-skeleton.component';
+import { PokemonsService } from '../../pokemons/services/pokemons.service';
 
 @Component({
   selector: 'pokemons-page',
@@ -9,11 +18,28 @@ import { PokemonListSkeletonComponent } from '../../pokemons/components/pokemon-
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export default class PokemonsPageComponent implements OnInit {
-  public isLoading = signal(true);
+  // public isLoading = signal(true);
+
+  // private appRef = inject(ApplicationRef);
+  private pokemonsService = inject(PokemonsService);
+
+  // private $appState = this.appRef.isStable.subscribe((isStable) => {
+  //   console.log({ isStable });
+  // });
 
   ngOnInit(): void {
-    setTimeout(() => {
-      this.isLoading.set(false);
-    }, 1500);
+    // setTimeout(() => {
+    //   this.isLoading.set(false);
+    // }, 5000);
+    this.loadPokemons(0);
+  }
+  // ngOnDestroy(): void {
+  //   this.$appState.unsubscribe();
+  // }
+
+  loadPokemons(page = 0) {
+    this.pokemonsService.loadPage(page).subscribe((resp) => {
+      console.log('OnInit');
+    });
   }
 }
---
+++ _pro/02-pokemon-ssr/src/app/pokemons/services/pokemons.service.ts
@@ -0,0 +1,33 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { map, Observable, tap } from 'rxjs';
+import { SimplePokemon } from '../interfaces/simple-pokemon.interface';
+import { PokemonsResponseAPI } from '../interfaces/pokemons-response-api.interface';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class PokemonsService {
+  private http = inject(HttpClient);
+
+  public loadPage(page: number): Observable<SimplePokemon[]> {
+    if (page !== 0) {
+      --page;
+    }
+
+    page = Math.max(0, page);
+
+    return this.http
+      .get<PokemonsResponseAPI>(`https://pokeapi.co/api/v2/pokemon?offset=${page * 20}&limit=20`)
+      .pipe(
+        map((resp) => {
+          const simplePokemons: SimplePokemon[] = resp.results.map((pokemon) => ({
+            id: pokemon.url.split('/').at(-2) ?? '',
+            name: pokemon.url,
+          }));
+          return simplePokemons;
+        }),
+        tap(console.log)
+      );
+  }
+}
