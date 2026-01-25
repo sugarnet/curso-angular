bac040eab04f1d08aee52b34531a08277d7af06c
Diego Scifo <diegoscifo@yahoo.com.ar>
2026-01-25 11:06:20 -0300

    refactor: remove unused files and components from pokemon-ssr project
    - Deleted package.json, favicon.ico, and various configuration files.
    - Removed all application components, services, and interfaces related to Pokémon functionality.
    - Cleaned up HTML templates and stylesheets.
    - Eliminated server-side rendering configurations and main application entry points.

--- _pro/pokemon-ssr/src/app/pokemons/components/pokemon-list-skeleton/pokemon-list-skeleton.component.ts
+++
@@ -1,9 +0,0 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-
-@Component({
-  selector: 'pokemon-list-skeleton',
-  imports: [],
-  templateUrl: './pokemon-list-skeleton.component.html',
-  changeDetection: ChangeDetectionStrategy.OnPush,
-})
-export class PokemonListSkeletonComponent {}
--- _pro/pokemon-ssr/src/app/pokemons/interfaces/simple-pokemon.interface.ts
+++
@@ -1,4 +0,0 @@
-export interface SimplePokemon {
-  name: string;
-  id: string;
-}
