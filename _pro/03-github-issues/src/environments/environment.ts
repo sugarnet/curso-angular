ddec9eef426ca0800ccdcf4ca76e62f67dc40c1e
Diego Scifo <diego.scifo@globallogic.com>
2026-02-06 08:33:06 -0300

    feat: remove GitHub token from environment configuration for security

--- _pro/03-github-issues/src/environments/environment.ts
+++ _pro/03-github-issues/src/environments/environment.ts
@@ -1,4 +1,3 @@
 export const environment = {
   baseUrl: `https://api.github.com/repos/angular/angular`,
-  gitHubToken: `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`,
 };
